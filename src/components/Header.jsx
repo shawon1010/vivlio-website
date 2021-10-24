b8ca729bf8ea7ca84b25810a6c4ccb974648f98f
Shawon1010 <shawon.datascience@gmail.com>
2021-10-24 14:23:53 -0400

    link fix try 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,43 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 
 
 function Header() {
   return (
    
    <>
+   
   <Navbar bg="dark" variant="dark">
     <Container>
     <Navbar.Brand style = {{fontSize: '34px'}} href="/">[Vιvλίο]</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="/cs">Computer Science</Nav.Link>
-      <Nav.Link href="/neuroscience">Neuroscience</Nav.Link>
-      <Nav.Link href="/history">History</Nav.Link>
+     
+    <Link to="/cs"  style={{
+                color: `white`,
+                textDecoration: `none`, marginRight: `10px`
+              }}>
+             Computer Science
+             </Link>
+             <Link to="/neuroscience"  style={{
+                color: `white`,
+                textDecoration: `none`, marginRight: `10px`
+              }}>
+             Neuroscience
+             </Link>
+             <Link to="/history"  style={{
+                color: `white`,
+                textDecoration: `none`,marginRight: `10px`
+              }}>
+             History
+             </Link>
+     
+    
     </Nav>
     </Container>
   </Navbar>
+ 
 </>
     
            
