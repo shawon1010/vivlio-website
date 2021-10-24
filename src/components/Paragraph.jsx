3df55afa08f549d026d830009a519b80f71329b3
rjanamsetty <62188918+rjanamsetty@users.noreply.github.com>
2021-10-24 11:38:46 -0400

    partial Paragraph

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -6,34 +6,51 @@ function Paragraph(props) {
     const { h3, h5 } = props;
     const [show, setShow] = useState(false);
     const [language, setLanguage] = useState("es");
+    const [ph3, setph3] = useState("");
 
     let posth3 = "";
     let posth5 = "";
 
     function showText() {
-        setCategories(true);
+        setShow(true);
     }
 
     function hideText() {
-        setCategories(false);
+        setShow(false);
     }
 
-    function translateText() {
-        posth3 = translate(h3, language);
-        posth5 = translate(h5, language);
-    }
+    const translateText = () => {
+        setph3(translate(h3, language));
+        posth5 = translate(h5, language).translatedText;
+        console.log(ph3);
+        showText();
+    };
 
-    function summarizeText() {
+    const summarizeText = () => {
         posth3 = h3;
-        posth5 = summarize(h5, 30);
-    }
+        posth5 = summarize(h5, 50).summary;
+        showText();
+    };
+
+    const clear = () => {
+        posth3 = "";
+        posth5 = "";
+        hideText();
+    };
 
     return (
         <div>
             <h3>{h3}</h3>
             <h5>{h5}</h5>
-            <button onClick></button>
-            {show && <div></div>}
+            <button onClick={summarizeText}>Summarize</button>
+            <button onClick={translateText}>Translate</button>
+            {show && (
+                <div>
+                    {ph3.translatedText && <h3>{ph3.translatedText}</h3>}
+                    <h5>{posth5}</h5>
+                    <button onClick={clear}>Clear</button>
+                </div>
+            )}
         </div>
     );
 }
